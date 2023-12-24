dd405feee2ef3df2b9266023e7492d938edbbbf8
Somnath Golui <code.somu@gmail.com>
2023-12-25 01:07:02 +0530

    Wrote a express server, userRouter, scripts and added prettierrc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello");
+import express from "express";
+import type { Request, Response, Express } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { userRouter } from "./routes/user";
+
+dotenv.config({
+  override: true,
+  path: path.join(__dirname, "../.env"),
+})
+
+const app: Express = express();
+app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.get("/ping", async (_req: Request, res: Response) => {
+  try {
+    res.send("pong");
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+
+  }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Express server is listenng on http://localhost:${8080}`)
+})
---
+++ server/src/routes/user.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import type { Request, Response } from "express";
+
+export const userRouter: Router = Router();
+
+userRouter.get("/", async (_req: Request, res: Response) => {
+  res.send("user router working");
+})
