5a1126c69e8e57f09bf24145f62f7356eee0fb43
Somnath Golui <code.somu@gmail.com>
2023-12-25 10:33:09 +0530

    prisma client

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,7 +1,9 @@
+import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import type { Request, Response } from "express";
 
 export const userRouter: Router = Router();
+const prisma = new PrismaClient();
 
 userRouter.get("/", async (_req: Request, res: Response) => {
   res.send("user router working");
