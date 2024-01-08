33182a26143fb4f304bf1129e2ba9a12a503e6f4
Somnath Golui <code.somu@gmail.com>
2024-01-09 01:57:09 +0530

    Created Navbar and added to root layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { authOptions } from "../utils/auth";
+import SignoutButton from "./LogoutButton";
+
+export async function Navbar() {
+  const session = await getServerSession(authOptions);
+  return (
+    <header>
+      <nav className="max-w-7xl mx-auto flex flex-row justify-between items-center m-6">
+        <div className="left-nav">
+          <Link href="/">
+            <div className="text-xl font-semibold">
+              Campus Ghost
+            </div>
+          </Link>
+        </div>
+        <div className="right-nav">
+          {session ? (
+            <div className="flex flex-row items-center gap-4">
+              <h1 className="text-lg font-medium">Account</h1>
+              <SignoutButton />
+            </div>
+          ) : (
+
+            <Button asChild className="text-lg font-medium">
+              <Link href="/auth">
+                SignIn
+              </Link>
+            </Button>
+
+          )}
+        </div>
+      </nav>
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,12 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./utils/auth"
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import SignoutButton from "./components/LogoutButton";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main>
+    <div>
       <h1 className='text-2xl text-center mt-8 font-semibold'>Hello from index page, this is a public route.</h1>
       <h2 className="text-xl text-center mt-2 font-medium">We need to update the UI of this page.</h2>
-      {
-        session ? (
-          <div>
-            <h1>You are authenticated</h1>
-            <SignoutButton />
-          </div>
-        ) : (
-          <div className="">
-            <h1>Signin here</h1>
-            <Button asChild>
-              <Link href={"/auth"}>Signin</Link>
-            </Button>
-          </div>
-        )
-      }
-    </main>
+    </div>
   )
 }
